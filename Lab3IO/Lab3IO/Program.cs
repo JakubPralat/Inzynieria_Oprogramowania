e7ef56c7b64a701c35544765ed763c703e3621da
JakubPralat <33075420+JakubPralat@users.noreply.github.com>
2017-12-20 00:54:52 +0100

    Lab 3

---
+++ Lab3IO/Lab3IO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Lab3IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZadaniaTAP zadania = new ZadaniaTAP();
+            Task<XmlDocument> task = zadania.Zadanie3("http://www.feedforall.com/sample.xml");
+            Task.WhenAll(task);
+            XmlDocument xmlDoc = task.GetAwaiter().GetResult();
+        }
+
+    }
+}

040b0b3bd3e0c67decca184b869539f9479308eb
JakubPralat <33075420+JakubPralat@users.noreply.github.com>
2018-01-17 09:26:34 +0100

    Update Program.cs

--- Lab4IO/Lab4IO/Program.cs
+++ Lab4IO/Lab4IO/Program.cs
@@ -14,16 +14,23 @@ namespace Lab4IO
         {
             Server s = new Server();
             s.Run();
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            Task.WaitAll(s.ServerTask);
-
+            
+            Client client1 = new Client();
+            Client client2 = new Client();
+            client1.Connect();
+            client2.Connect();
+            
+            CancellationTokenSource cancellationToken1 = new CancellationTokenSource();
+            CancellationTokenSource cancellationToken2 = new CancellationTokenSource();
+            
+            
+            var zm1 = client.keepPinging("Wiadomosc Client1", cancellationToken1.Token);
+            var zm2 = client2.keepPinging("Wiadomosc Client2", cancellationToken2.Token);
+            
+            cancellationToken1.CancelAfter(3000);
+            cancellationToken2.CancelAfter(4000);
+            Task.WaitAll(new Task[] {zm1, zm2 });
+            
         }
     }
 }
